d95aac8b64cd02a5287d2298fbbef8e3df6e2368
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2021-12-27 17:05:14 -0300

    Start saving logs

--- src/controller/log.mjs
+++ src/controller/log.mjs
@@ -1,15 +1,27 @@
+import { logPath } from '../../config/default.mjs';
+import { writeFile } from 'fs/promises';
 import { createInitialLine, findNonce, buildCsvLine } from '../lib/logHelpers.mjs';
 
-const log = (req, res) => {
+const log = async (req, res) => {
   const message = req.body.message.trim();
+  let csvLine = null;
 
   if (! req.body.prevSha256) {
-    const initialLine = createInitialLine(message);
-    res.status(200).send(initialLine);
+    csvLine = createInitialLine(message);
   } else {
     const sha256 = req.body.prevSha256.trim();
     const nonce = findNonce({ sha256, message });
-    res.status(200).send(buildCsvLine({ sha256, message, nonce }));
+    csvLine = buildCsvLine({ sha256, message, nonce });
+  }
+
+  try {
+    await writeFile(logPath, csvLine, { flag: 'a'});
+    return res.status(200).end();
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({
+      message: `Couldn't write to log file`,
+    });
   }
 };
 
